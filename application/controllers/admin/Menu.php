0a2b9a78c0293c649f31a70a042ddbbb77311009
Fitriansyah1 <fitriansyahh413@gmail.com>
2022-05-22 14:46:44 -0700

    Disposisi surat selesai

--- application/controllers/admin/Menu.php
+++ application/controllers/admin/Menu.php
@@ -1,12 +1,21 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Menu extends CI_Controller {
+class Menu extends CI_Controller
+{
     public function index()
-	{
-		$data = array(
-			'content'=> 'dashboard/index.php'
-		);
-		$this->load->view('template/menu');
-	}
+    {
+        $data = array(
+            'content' => 'dashboard/index.php'
+        );
+        $this->load->view('template/main.php', $data);
+    }
+    public function add()
+    {
+        $this->load->view('admin/user/add_form.php');
+    }
+    public function edit()
+    {
+        $this->load->view('admin/user/edit_form.php');
+    }
 }
